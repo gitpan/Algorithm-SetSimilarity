60050b5f8d4acd40a27c9cf3e690aab74ab1ee66
Toshinori Sato <overlast@cpan.org>
2013-09-22 08:50:02 +0000

    Import of OVERLAST/Algorithm-SetSimilarity-0.0.0_02 from CPAN.
    gitpan-cpan-distribution: Algorithm-SetSimilarity
    gitpan-cpan-version:      0.0.0_02
    gitpan-cpan-path:         OVERLAST/Algorithm-SetSimilarity-0.0.0_02.tar.gz
    gitpan-cpan-author:       OVERLAST
    gitpan-cpan-maturity:     developer

--- lib/Algorithm/SetSimilarity/Join/Datum.pm
+++ lib/Algorithm/SetSimilarity/Join/Datum.pm
@@ -4,14 +4,15 @@
  use strict;
  use warnings;
 
- our $VERSION = "0.0.0_01";
+ our $VERSION = "0.0.0_02";
+
+ use Scalar::Util;
 
  sub new {
      my ($class, $param) = @_;
      my @array = ();
      my %hash = (
          "datum" => \@array,
-         "data_type" => "string",
      );
      $hash{"data_type"} = $param->{"data_type"} if (exists $param->{"data_type"});
      bless \%hash, $class;
@@ -44,12 +45,38 @@
      return $is_update;
  }
 
+ sub estimate_data_type {
+     my ($self, $set) = @_;
+     my $is_estimate = -1;
+     my $type = "";
+     my $max_check_elements = 5;
+     for (my $i = 0; ($i <= $#$set) && ($i < $max_check_elements); $i++) {
+         my $tmp_type = "";
+         if (Scalar::Util::looks_like_number($set->[$i])) {
+             $tmp_type = "number";
+         } else {
+             $tmp_type = "string";
+         }
+         if ($type eq "") {
+             $type = $tmp_type;
+             $is_estimate = 1;
+         } elsif ($type ne $tmp_type) {
+             $is_estimate = 0;
+             last;
+         }
+
+     }
+     $self->{data_type} = $type if ($is_estimate);
+     return $is_estimate;
+ }
+
  sub sort_set {
      my ($self, $set) = @_;
      my @array = ();
      if ((defined $set) && (ref $set eq "ARRAY")) {
          @array = @{$set};
          if ($#$set > 0) {
+             $self->estimate_data_type($set) unless (exists $self->{data_type});
              if ($self->{data_type} eq "number") {
                  @array = sort { $a <=> $b } @{$set};
              }
